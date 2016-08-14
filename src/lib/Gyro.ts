8178819f33aa4ebb553805cbad2937cc4f363dc8
cleanmind777 <kanaogaki22@gmail.com>
2016-08-13 23:36:07 -0300

    Add comments and update the Gyro Class

--- src/lib/Gyro.ts
+++ src/lib/Gyro.ts
@@ -30,46 +30,69 @@ export default class Gyro
 
 	constructor()
 	{
-		console.log("INIT GYRO");
-
 		this.gyro.frequency = 1000;
 
-		window.addEventListener('MozOrientation', this.mozOrientationInitListener.bind(this), true);
-		window.addEventListener('devicemotion', this.deviceMotionListener.bind(this), true);
-		window.addEventListener('deviceorientation', this.deviceOrientationListener.bind(this), true);
-
+		if (window && window.addEventListener) {
+			window.addEventListener('MozOrientation', this.mozOrientationInitListener.bind(this), true);
+			window.addEventListener('devicemotion', this.deviceMotionListener.bind(this), true);
+			window.addEventListener('deviceorientation', this.deviceOrientationListener.bind(this), true);
+		}
 	}
 
+	/**
+	 * Calibrate
+	 *
+	 * @method calibrate
+	 * @return {void}
+	 **/
 	public calibrate() {
-		for (var i in this.measurements) {
+		for (let i in this.measurements) {
 			this.calibration[i] = (typeof this.measurements[i] === 'number') ? this.measurements[i] : 0;
 		}
 	};
 
+	/**
+	 * Return the orientation of the device
+	 *
+	 * @method getOrientation
+	 * @return {void}
+	 **/
 	public getOrientation() {
 		return this.measurements;
 	};
 
+	/**
+	 * Begin the tracking
+	 *
+	 * @method startTracking
+	 * @param {any} callback
+	 * @return {void}
+	 **/
 	public startTracking(callback) {
 		this.interval = setInterval(() => {
 			callback(this.measurements);
 		}, this.gyro.frequency);
 	};
 
+	/**
+	 * Stop the tracking
+	 *
+	 * @method stopTracking
+	 * @return {void}
+	 **/
 	public stopTracking() {
 		clearInterval(this.interval);
 	};
 
-
 	/**
-	 * Current available features are:
-	 * MozOrientation
-	 * devicemotion
-	 * deviceorientation
-	 */
+	 * [...]
+	 *
+	 * @method hasFeature
+	 * @param {any} feature (MozOrientation, devicemotion, or deviceorientation)
+	 * @return {boolean}
+	 **/
 	public hasFeature(feature) {
-		console.log("feature", feature);
-		for (var i in this.features) {
+		for (let i in this.features) {
 			if (feature == this.features[i]) {
 				return true;
 			}
@@ -77,29 +100,48 @@ export default class Gyro
 		return false;
 	};
 
+	/**
+	 * Return the features of the device
+	 *
+	 * @method getFeatures
+	 * @return {void}
+	 **/
 	public getFeatures() {
 		return this.features;
 	};
 
 
+	/**
+	 * Conversion Euler to Quaternion
+	 *
+	 * @method eulerToQuaternion
+	 * @param {any} e
+	 * @return {void}
+	 **/
 	private eulerToQuaternion(e) {
-		var s:any = Math.PI / 180;
-		var x:any = e.beta * s, y = e.gamma * s, z = e.alpha * s;
-		var cX:any = Math.cos(x / 2);
-		var cY:any = Math.cos(y / 2);
-		var cZ:any = Math.cos(z / 2);
-		var sX:any = Math.sin(x / 2);
-		var sY:any = Math.sin(y / 2);
-		var sZ:any = Math.sin(z / 2);
-		var w:any = cX * cY * cZ - sX * sY * sZ;
+		let s:any = Math.PI / 180;
+		let x:any = e.beta * s, y = e.gamma * s, z = e.alpha * s;
+		let cX:any = Math.cos(x / 2);
+		let cY:any = Math.cos(y / 2);
+		let cZ:any = Math.cos(z / 2);
+		let sX:any = Math.sin(x / 2);
+		let sY:any = Math.sin(y / 2);
+		let sZ:any = Math.sin(z / 2);
+		let w:any = cX * cY * cZ - sX * sY * sZ;
 		x = sX * cY * cZ - cX * sY * sZ;
 		y = cX * sY * cZ + sX * cY * sZ;
 		z = cX * cY * sZ + sX * sY * cZ;
 		return {x:x, y:y, z:z, w:w};
 	}
 
-	//this.gyro.eulerToQuaternion=this.eulerToQuaternion;
-
+	/**
+	 * Multiplies two quaternions
+	 *
+	 * @method quaternionMultiply
+	 * @param {any} a
+	 * @param {any} b
+	 * @return {void}
+	 **/
 	private quaternionMultiply(a, b) {
 		return {
 			w: a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z,
@@ -109,37 +151,58 @@ export default class Gyro
 		};
 	}
 
+	/**
+	 * Set the quaternion
+	 *
+	 * @method quaternionApply
+	 * @param {any} v
+	 * @param {any} a
+	 * @return {void}
+	 **/
 	private quaternionApply(v, a) {
 		v = this.quaternionMultiply(a, {x:v.x,y:v.y,z:v.z,w:0});
 		v = this.quaternionMultiply(v, {w:a.w, x:-a.x, y:-a.y, z:-a.z});
 		return {x:v.x, y:v.y, z:v.z};
 	}
 
+	/**
+	 * Return the vector (for the gamma)
+	 *
+	 * @method vectorDot
+	 * @param {any} a
+	 * @param {any} b
+	 * @return {void}
+	 **/
 	private vectorDot(a, b) {
 		return a.x * b.x + a.y * b.y + a.z * b.z;
 	}
 
+	/**
+	 * Conversion Quaternion to Euler
+	 *
+	 * @method quaternionToEuler
+	 * @param {any} q
+	 * @return {void}
+	 **/
 	private quaternionToEuler(q) {
-		var s = 180 / Math.PI;
-		var front = this.quaternionApply({x:0,y:1,z:0}, q);
-		var alpha = (front.x == 0 && front.y == 0) ?
+		let s = 180 / Math.PI;
+		let front = this.quaternionApply({x:0,y:1,z:0}, q);
+		let alpha = (front.x == 0 && front.y == 0) ?
 			0 : -Math.atan2(front.x, front.y);
-		var beta = Math.atan2(front.z,Math.sqrt(front.x*front.x+front.y*front.y));
-		var zgSide = {
+		let beta = Math.atan2(front.z,Math.sqrt(front.x*front.x+front.y*front.y));
+		let zgSide = {
 			x: Math.cos(alpha),
 			y: Math.sin(alpha),
 			z: 0
 		};
-		var zgUp = {
+		let zgUp = {
 			x: Math.sin(alpha) * Math.sin(beta),
 			y: -Math.cos(alpha) * Math.sin(beta),
 			z: Math.cos(beta)
 		};
-		var up = this.quaternionApply({x:0,y:0,z:1}, q);
-		var gamma = Math.atan2(this.vectorDot(up, zgSide), this.vectorDot(up, zgUp));
+		let up = this.quaternionApply({x:0,y:0,z:1}, q);
+		let gamma = Math.atan2(this.vectorDot(up, zgSide), this.vectorDot(up, zgUp));
 
-		// wrap-around the value according to DeviceOrientation
-		// Event Specification
 		if (alpha < 0) alpha += 2 * Math.PI;
 		if (gamma >= Math.PI * 0.5) {
 			gamma -= Math.PI; alpha += Math.PI;
@@ -154,6 +217,13 @@ export default class Gyro
 		return {alpha: alpha * s, beta: beta * s, gamma: gamma * s};
 	}
 
+	/**
+	 * Add an eventlistener
+	 *
+	 * @method mozOrientationInitListener
+	 * @param {any} e
+	 * @return {void}
+	 **/
 	public mozOrientationInitListener(e):void
 	{
 		this.features.push('MozOrientation');
@@ -166,6 +236,13 @@ export default class Gyro
 		}, true);
 	}
 
+	/**
+	 * Add an eventlistener
+	 *
+	 * @method deviceMotionListener
+	 * @param {any} e
+	 * @return {void}
+	 **/
 	public deviceMotionListener(e):void
 	{
 		this.features.push('devicemotion');
@@ -178,24 +255,31 @@ export default class Gyro
 		}, true);
 	}
 
+	/**
+	 * Add an eventlistener
+	 *
+	 * @method deviceOrientationListener
+	 * @param {any} e
+	 * @return {void}
+	 **/
 	public deviceOrientationListener(e):void
 	{
 		this.features.push('deviceorientation');
 		e.target.removeEventListener('deviceorientation', this.deviceOrientationListener, true);
 
 		e.target.addEventListener('deviceorientation', (e) => {
-			var calib = this.eulerToQuaternion({
+			let calib = this.eulerToQuaternion({
 				alpha: this.calibration.rawAlpha,
 				beta: this.calibration.rawBeta,
 				gamma: this.calibration.rawGamma
 			});
 			calib.x *= -1; calib.y *= -1; calib.z *= -1;
 
-			var raw = this.eulerToQuaternion({
+			let raw = this.eulerToQuaternion({
 				alpha: e.alpha, beta: e.beta, gamma: e.gamma
 			});
-			var calibrated = this.quaternionMultiply(calib, raw);
-			var calibEuler = this.quaternionToEuler(calibrated);
+			let calibrated = this.quaternionMultiply(calib, raw);
+			let calibEuler = this.quaternionToEuler(calibrated);
 
 			this.measurements.alpha = calibEuler.alpha;
 			this.measurements.beta = calibEuler.beta;
